c251156b7f92c0ef7c91de2abc83a9ad157602e5
Angelo Corsaro <angelo@icorsaro.net>
2012-10-25 14:51:37 +0200

    Issue: 16402

--- src/hpp/dds/sub/LoanedSamples.hpp
+++ src/hpp/dds/sub/LoanedSamples.hpp
@@ -11,11 +11,22 @@ namespace dds {
     template <typename T,
     template <typename Q> class DELEGATE = detail::LoanedSamples>
     class LoanedSamples;
+
+    // Used by C++11 compilers to allow for using LoanedSamples
+    // and SharedSamples in a range based for-loop.
+    template <typename T> typename T::iterator begin(T& t);
+    template <typename T> typename T::iterator end(T& t);
+
+    template <typename T> typename T::const_iterator begin(const T& t);
+    template <typename T> typename T::const_iterator end(const T& t);
   } 
 }
 
 /**
  * This class encapsulate and automates the management of loaned samples.
+ * Notice that this class should be <b>exception safe</b> and implement
+ * the move idiom as explained at:
+ * http://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Move_Constructor
  */
 template <typename T,
 template <typename Q> class DELEGATE>
